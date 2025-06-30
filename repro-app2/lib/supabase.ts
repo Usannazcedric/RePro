0ebbb5570cbf17ef02f1bc5184922f57cd8031f9
Usannazcedric <cedric.usannaz-joris@epitech.digital>
2025-07-01 01:24:01 +0200

    Ignore node_modules

--- repro-app2/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://etfsygfgkktxawffzxoe.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZnN5Z2Zna2t0eGF3ZmZ6eG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MzUxMTAsImV4cCI6MjA2NTMxMTExMH0.ZwasUMvUhp67ZTNBgbbcCtAvmNUavKZ2vaEX5ZRPoJg';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
